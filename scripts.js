3788f625f567596ba98bdbb121ef78184efc6c0a
Konstantin <kostyasupikos@gmail.com>
2018-12-19 00:40:36 +0300

    adding toCamelCase function

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
 function splitAndMerge(str,sp) {
 	return str.split('').join(sp);
 }
+
+function toCamelCase(str) {
+    return str.split('-').map((element,index) => {
+    	return index === 0 ? element : element.replace(element[0],element[0].toUpperCase());
+    }).join('_');
+}
