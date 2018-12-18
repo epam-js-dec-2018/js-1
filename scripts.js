c6008b27dc940e5ea7b88ee141e4d0b76b4c997a
Konstantin <kostyasupikos@gmail.com>
2018-12-19 00:45:13 +0300

    adding reverseWordsInSentence function

--- scripts.js
+++ scripts.js
@@ -1,9 +1,18 @@
+// 1
 function splitAndMerge(str,sp) {
 	return str.split('').join(sp);
 }
 
+// 3
 function toCamelCase(str) {
     return str.split('-').map((element,index) => {
     	return index === 0 ? element : element.replace(element[0],element[0].toUpperCase());
     }).join('_');
+}
+
+// 4
+function reverseWordsInSentence(str) {
+    return str.split(' ').map((element) => {
+        return element.split('').reverse().join('');
+    }).join(' ');
 }
