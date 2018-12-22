a2efaa0d64db391140a1ea391c71795be7cd4bc5
AndreyS55 <storozhko-a-v@mail.ru>
2018-12-23 01:12:26 +0300

    complete homework

--- Andrey Storozhko/index.js
+++ Andrey Storozhko/index.js
@@ -1,16 +1,13 @@
 //------------- ** 1 ** -------------//
 
 function splitAndMerge(str, sp) {
-    if (typeof str !== 'string') {
-        console.log('First argument must be a string!');
-    } else {
-        var words = str.split(' ');
-        var splitWords = words.map(function(word) {
-            return word.split('').join(sp);
-        });
-        return splitWords.join(' ');
-    }
+    return str.split(' ').map(function(word) {
+        return word.split('').join(sp);
+    }).join(' ');
 }
+
+console.log(splitAndMerge("My name is John"," "));
+console.log(splitAndMerge("Hello World!",","));
 
 //------------- ** 2 ** -------------//
 
@@ -22,28 +19,38 @@ function convert(hash) {
     return arr;
 }
 
+console.log(convert({name: 'Jeremy', age: 24, role: 'Software Engineer'}));
+
 //------------- ** 3 ** -------------//
 
 function toCamelCase(str) {
     return str.replace(/[-_]/g, '');
 }
 
+console.log(toCamelCase("the-stealth-warrior"));
+console.log(toCamelCase("The_Stealth_Warrior"));
+
 //------------- ** 4 ** -------------//
 
 function reverseSentence(str) {
-    return str.split("").reduce(function(rev, char) {
-        return char + rev;
-    }, '');
+    return str.split(' ').map(function(word) {
+        return word.split('').reverse().join('');
+    }).join(' ');
 }
 
-function reverseSentence(str) {
-    return str.split("").reverse().join("");
-}
+console.log(reverseSentence(" A fun little challenge! "));
 
 //------------- ** 5 ** -------------//
 
+function stringExpansion(str) {
+    return str.replace(/(\d)([^\d]*)/g, function(match, p1, p2) {
+        return p2.repeat(p1);
+    })
+}
 
-
+console.log(stringExpansion('3D2a5d2f'));
+console.log(stringExpansion('3d332f2a'));
+console.log(stringExpansion('abcde'));
 
 //------------- ** 6 ** -------------//
 
@@ -57,9 +64,59 @@ function smallest() {
     return Math.min.apply(null, args);
 }
 
+console.log(largest(2, 0.1, -5, 100, 3));
+console.log(smallest(2, 0.1, -5, 100, 3));
+
 //------------- ** 7 ** -------------//
 
 function transform(arr) {
-
+    return arr.map(function(item) {
+        return function() {
+            return item;
+        };
+    });
 }
 
+const baseArray = [10, 20, 30, 40, 50];
+const newArray = transform(baseArray);
+console.log(newArray[3]());
+console.log(newArray[4]());
+
+//------------- ** 8 ** -------------//
+
+function sum(num) {
+    if(arguments.length === 1) {
+        return num;
+    } else {
+        return num + sum.apply(null,[].slice.call(arguments,1));
+    }
+}
+
+console.log(sum(1,3,5,7));
+
+//------------- ** 9 ** -------------//
+
+function countDown(num) {
+    var timer = setInterval(function() {
+        console.log(num);
+        num--;
+        if (num < 0) clearInterval(timer);
+    }, 1000);
+}
+
+console.log(countDown(3));
+
+//------------- ** 10 ** -------------//
+
+Function.prototype.myBind = function () {
+    var func = this,
+        args = [].slice.call(arguments),
+        context = args.shift();
+    return function () {
+        return func.apply(context, args.concat([].slice.call(arguments)));
+    };
+};
+
+function addPropToNumber(number) { return this.prop + number; }
+var bound = addPropToNumber.myBind({ prop: 9 });
+bound(1);
