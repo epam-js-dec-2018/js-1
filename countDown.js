e76329deaf5c1aa6e2ffc60d8841439803a538df
Varvara Ingilevich <v_ingilevich@niuitmo.ru>
2019-01-06 00:41:10 +0300

    countDown

--- countDown.js
+++ countDown.js
@@ -1,5 +1,12 @@
-function countDown(num) {
-    Array.apply(null, {length: num}).map(Number.call, Number).map(function (elem) {
-        
-    })
+function countDown(n) {
+    let i = n;
+    let timerId = setInterval(function() {
+        console.info(i);
+        if (i === 0) {
+            clearInterval(timerId);
+        }
+        i--;
+    }, 2000);
 }
+
+countDown(5;
